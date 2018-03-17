52b2242c205ad91363ce27196fa76265e8aabcec
KHB <kiel.byrne@gmail.com>
2018-03-17 11:01:16 -0400

    styled map, got markers added

--- imports/client/MyMap.js
+++ imports/client/MyMap.js
@@ -11,10 +11,50 @@ class MyMap extends Component {
   }
 
   handleMapOptions() {
-    return {
-      center: new google.maps.LatLng(38.889931, -77.009003),
-      zoom: 12,
-    };
+            let mapCenter
+        if (!Session.get('browserLoc')) {
+            mapCenter = {'lat':38.902635, 'lng':-77.022938};
+        } else {
+            mapCenter = Session.get('browserLoc');
+            // MAP_ZOOM = 12;
+            console.log("Got mapCenter from Browser:", mapCenter);
+        }
+            return {
+                // ============================= RETURN MAP OPTIONS ==================================    
+                center: new google.maps.LatLng(mapCenter),
+                zoom: 13,
+                minZoom: 11,
+                maxZoom: 17,
+                // mapTypeId:google.maps.MapTypeId.TERRAIN,
+                backgroundColor: "#555",
+                clickableIcons: false,
+                disableDefaultUI: true,
+                fullscreenControl: false,
+                zoomControl: true,
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.RIGHT_BOTTOM
+                },
+                mapTypeControl: false,
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                    position: google.maps.ControlPosition.RIGHT_CENTER,
+                    // mapTypeIds: ['roadmap', 'terrain']
+                },
+                scaleControl: false,
+                rotateControl: true,
+                streetViewControl: false,
+                streetViewControlOptions: {
+                    position: google.maps.ControlPosition.BOTTOM_CENTER
+                },
+                //gestureHandling sets the mobile panning on a scrollable page: COOPERATIVE, GREEDY, AUTO, NONE
+                gestureHandling: 'greedy',
+                // Map styles; snippets from 'Snazzy Maps'.
+                styles: 
+                    // lightGray 
+                    // [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#fefefe"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#b2b2b2"}]},{"featureType":"administrative.country","elementType":"labels.text.stroke","stylers":[{"color":"#ff0000"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative.locality","elementType":"labels.text.stroke","stylers":[{"color":"#696969"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.stroke","stylers":[{"color":"#696969"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#aaaaaa"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#b3b3b3"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#4f4f4f"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#6a6a6a"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#dbdbdb"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]}]
+                    // greyMonochrome
+                    [{"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c8d7d4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}]
+            };
   }
 
   handleOnReady(name) {
@@ -62,7 +102,7 @@ class MyMap extends Component {
 
         Events.find().observeChanges({
           added: function(id,doc) {
-            console.log(doc);
+            // console.log(doc);
 
             let latLng = doc.eventAddress.coords.split(",");
             let lat = Number(latLng[0]);
@@ -81,13 +121,15 @@ class MyMap extends Component {
 
             const marker = new google.maps.Marker({
               animation: google.maps.Animation.DROP,
-              position: new google.maps.LatLng(document.lat, document.lng),
+              position: latLngObj,
               map: map.instance,
               icon: eventImage,
-              id: document._id,
+              id: id,
             });
 
-            markers[document._id] = marker;
+
+            markers[id] = marker;
+            console.log(markers);
           },
           changed: function(newDocument, oldDocument) {
             markers[newDocument._id].setPosition({
