b20366774845e8f399b15ad58d51c9344e87512c
KHB <kiel.byrne@gmail.com>
2018-03-30 02:39:24 -0400

    updated pw again

--- imports/startup/server/SMTP.js
+++ imports/startup/server/SMTP.js
@@ -2,10 +2,9 @@
 const smtp = {
   // username: 'postmaster@sandbox6ba51b6be4634d88b492b2418b1b7c4c.mailgun.org',
   username: "noreply@pakke.us",
-  password: 'kr0wnedKing!',
+  password: 'pakkeP@RTY!!',
   server:   "smtp.zoho.com",
   port: 465
 };
 
 process.env.MAIL_URL = `smtps://${encodeURIComponent(smtp.username)}:${encodeURIComponent(smtp.password)}@${encodeURIComponent(smtp.server)}:${smtp.port}`;
-pakkeP@RTY!!!
