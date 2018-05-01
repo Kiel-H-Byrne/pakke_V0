cf5824d71aaf57b4729855d39f5abe5f7a7ac4c4
zachclem42@gmail.com <ZCC@ZachsMachine.fios-router.home>
2018-05-01 07:22:53 -0400

    add how it works sections

--- imports/client/pageLanding/ValueProp.js
+++ imports/client/pageLanding/ValueProp.js
@@ -12,7 +12,7 @@ class ValueProp extends Component {
     
         return (
             <div className='what-is-pakke'>
-                <h2 style={myStyle}> What is Pakke? </h2>
+                <h2 style={myStyle}> How it works </h2>
                 <div className='value-prop'>
 
 
@@ -21,10 +21,10 @@ class ValueProp extends Component {
                         <div className='idea'>
                             <Link className='value-prop-link'to='/events'>
                                 <div className='idea-border'>
-                                    <div className='glyphicon glyphicon-glass'></div>
-                                    <h3>Discover</h3>
+                                <img src="one.svg" alt="ticket" height="42" width="42"></img>
+                                    <h3>1. Apply for Tickets</h3>
                                 </div>
-                                <p> Pakke’s vision is to focus on the overall experience of how we socialize and more importantly, where and with who. As a guest, this is your opportunity to meet new people, learn about your city, and save money.</p>
+                                <p> It's free to apply. Just sign in.</p>
                                 
 
 
@@ -34,12 +34,10 @@ class ValueProp extends Component {
                         <div className='idea'>
                             <Link className='value-prop-link' to='/host'>
                                 <div className='idea-border'>
-                                    <div className='glyphicon glyphicon-home'></div>
-                                    <h3> Connect </h3>
+                                <img src="two.svg" alt="ticket" height="42" width="42"></img>
+                                    <h3>2. Get Selected </h3>
                                 </div>
-                                <p>
-                                    The physical environment is just as important to a day or night out that, if done right, we are willing to open our doors as hosts for curated events. For all hosts, there will be a dedicated experience curator that will provide professional support so your event is successful and profitable.
-                                    </p>
+                                <p>Selections are made using a lottory system. If you are selected, you’ll receive an email to purchase your tickets.</p>
 
                             </Link>
                         </div>
@@ -47,11 +45,10 @@ class ValueProp extends Component {
                         <div className='idea'>
                             <Link className='value-prop-link' to='/talent'>
                                 <div className='idea-border'>
-                                    <div className='glyphicon glyphicon-music'></div>
-                                    <h3> Experience </h3>
+                                <img src="three.svg" alt="ticket" height="42" width="42"></img>
+                                    <h3>3. Buy Tickets </h3>
                                 </div>
-                                <p> Pakke believes in the power of experiences but finding ways to tap into a unique social outting, we need creative people to guide us there. This is why we pay very close attention to our talented professionals who choose Pakke as a way to bring their art, craft or trade to our communities. Join us today and see how your talent can earn recognition and money.
- </p>
+                                <p>Purchase your tickets for yourself and friends. Then we’ll email you the event location a day before the event.</p>
                             </Link>
                         </div>
                     </ul>
