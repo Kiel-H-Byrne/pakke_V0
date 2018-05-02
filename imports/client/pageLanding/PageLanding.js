7d86946337ac3a2d84c9ec00be973644fc923eb0
zachclem42@gmail.com <ZCC@ZachsMachine.fios-router.home>
2018-05-01 22:28:36 -0400

    add what-is-pakke-event copy

--- imports/client/pageLanding/PageLanding.js
+++ imports/client/pageLanding/PageLanding.js
@@ -24,6 +24,11 @@ class LandingPage extends Component {
                     </div>
                 </div>
 
+                <div className='what-is-pakke-event'>
+                    <h3 className='what-is-pakke-event-text'>What is a Pakke event?</h3>
+                    <p className='what-is-pakke-event-text'>A Pakke event is a highly currated gettogether. Each event is specially taylored for people to connect and discover all the unique and special people that might otherwise not be possible. We encourage you to socialize in uniques spaces. These spaces are meant to help people make connect and socialize in ways that simply aren’t possible at bars and other traditional spaces.</p>
+                </div>
+
                 <div className="landingValue">
                     <HowItWorks />
                 </div>
