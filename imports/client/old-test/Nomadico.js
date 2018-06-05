f87b04c9e4da2a6713daead6db26d896eeef6afa
KHB <kiel.byrne@gmail.com>
2018-06-05 08:30:37 -0400

    nomadico details page

--- imports/client/old-test/Nomadico.js
+++ imports/client/old-test/Nomadico.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 // import { withTracker } from 'meteor/react-meteor-data';
-// import { BarLoader } from 'react-spinners';
+import { BarLoader } from 'react-spinners';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 
 class NomadicoDetails extends Component {
   constructor(props) {
@@ -16,12 +21,17 @@ class NomadicoDetails extends Component {
   render() {
     return (
       <div>
-        <CardMedia src='img/events/nomadico__cover.png' />
-        <h2>Meso Creso's Nomadico Festival</h2>
-        June 8-11
-       <address>   980 Cove Rd, Gore, VA 22637 </address>
-        <p>A festival celebrating global music, arts, and community featuring 40 DJs, workshops, yoga and wellness. Boho camping nestled in Virginia's Appalachian Mountains with immersive experiences and interactive artworks.</p>
-        <button></button>
+        
+        <Paper align={'center'} style={{ width: 960, margin:'auto', marginTop: '2%' }}>
+          <Typography gutterBottom variant="display4">Meso Creso's Nomadico Festival</Typography>
+        </Paper>
+        <a href="https://nomadicofestival.com/tickets/" target="_blank"><img src={'img/events/nomadico__cover.png'} /></a>
+        <Paper align={'center'} style={{ width: 360, margin: '2% auto'}}>
+       <Typography gutterBottom variant="display1"> 980 Cove Rd, Gore, VA 22637 </Typography>
+       <Typography gutterBottom variant=" body1"><em>A festival celebrating global music, arts, and community featuring 40 DJs, workshops, yoga and wellness. Boho camping nestled in Virginia's Appalachian Mountains with immersive experiences and interactive artworks.</em></Typography>
+        <Button size="large" color="secondary" href="https://nomadicofestival.com/tickets/" target="_blank">Get Tickets</Button>
+       </Paper>
+        
       </div>
     )
   }
