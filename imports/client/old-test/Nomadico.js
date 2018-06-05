93aa137ad031a363e835f30997b6e5b11172bcb0
KHB <kiel.byrne@gmail.com>
2018-06-05 10:04:49 -0400

    maxwidth image

--- imports/client/old-test/Nomadico.js
+++ imports/client/old-test/Nomadico.js
@@ -22,18 +22,23 @@ class NomadicoDetails extends Component {
   render() {
     return (
       <Grid container direction="column">
-        
-        <Paper align={'center'} style={{ width: '100%', margin:'auto', marginTop: '2%' }}>
-          <Typography gutterBottom variant="display4">Meso Creso's Nomadico Festival</Typography>
-          <a href="https://nomadicofestival.com/tickets/" target="_blank"><img src={'img/events/nomadico__cover.png'} /></a>
-        </Paper>
-        
-        <Paper align={'center'} style={{ width: 360, margin: '2% auto'}}>
-       <Typography gutterBottom variant="display1"> 980 Cove Rd, Gore, VA 22637 </Typography>
-       <Typography gutterBottom variant=" body1"><em>A festival celebrating global music, arts, and community featuring 40 DJs, workshops, yoga and wellness. Boho camping nestled in Virginia's Appalachian Mountains with immersive experiences and interactive artworks.</em></Typography>
-        <Button size="large" color="secondary" href="https://nomadicofestival.com/tickets/" target="_blank">Get Tickets</Button>
+        <Grid item xs={12}>
+          <Paper align={'center'} style={{ width:'80%', margin:'auto', marginTop: '2%' }}>
+            <Typography gutterBottom variant="display3">Meso Creso's Nomadico Festival</Typography>
+          </Paper>
+        </Grid>
+          <Grid item xs={12}>
+            <a href="https://nomadicofestival.com/tickets/" target="_blank">
+            <img src={'img/events/nomadico__cover.png'} style={{ width: '100%' }} /></a>
+         </Grid>
+       <Grid item xs={12}>
+        <Paper align={'center'} style={{ width: '80%', margin: '2% auto'}}>
+         <Typography gutterBottom variant="display1"> 980 Cove Rd, Gore, VA 22637 </Typography>
+         <Typography gutterBottom variant=" body1"><em>A festival celebrating global music, arts, and community featuring 40 DJs, workshops, yoga and wellness. Boho camping nestled in Virginia's Appalachian Mountains with immersive experiences and interactive artworks.</em></Typography>
+          <Button size="large" color="secondary" href="https://nomadicofestival.com/tickets/" target="_blank">Get Tickets</Button>
        </Paper>
-        
+      </Grid>
+
       </Grid>
     )
   }
