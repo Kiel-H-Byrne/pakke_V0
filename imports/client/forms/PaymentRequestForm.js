1af292531a8617cde0ce40d291eef3a2d9f037de
KHB <kiel.byrne@gmail.com>
2018-08-03 02:04:02 -0400

    minor

--- imports/client/forms/PaymentRequestForm.js
+++ imports/client/forms/PaymentRequestForm.js
@@ -136,7 +136,7 @@ class PaymentRequestForm extends React.Component {
   
 
   render() {
-    console.log(this.state)
+    // console.log(this.state)
     return this.state.canMakePayment ? (
       <PaymentRequestButtonElement
         paymentRequest={this.state.paymentRequest}
