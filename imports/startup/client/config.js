1661d5941773b56b0544f4a1a741e3a72112c4fa
Kiel Byrne <kiel.byrne@gmail.com>
2018-12-03 23:52:55 -0500

    perfect, but PACKAGE UP THE PACKAGE FOLDER AND SEND TO OTHER REPOS BEFORE PUBLISHING FROM OTHER PCS

--- imports/startup/client/config.js
+++ imports/startup/client/config.js
@@ -59,6 +59,9 @@ Accounts.ui.config({
   profilePath: '/profile',
   onSubmitHook: (error, state) => {
     console.log(error, state);
+    
+    // if (!error && state == "symbol(SIGN_IN)") { Bert.alert("Please check your e-mail to log-in without a password!", "pk-success", "growl-top-right", "fa-thumbs-up", )  }
+
     // if (state = "Symbol(SIGN_IN)") {
     //   console.log("true"); 
     //   return;
